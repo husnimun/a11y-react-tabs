7550cc0aca404a30d0947ac78a6657bdd6bb9b3d
Husni Munaya <husnimunaya@gmail.com>
2018-05-23 06:40:14 +0700

    Import PropTypes

--- src/components/TabList.js
+++ src/components/TabList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const DEFAULT_CLASS = 'a11y-react-tabs__tablist'
 
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import uuid from '../helpers/uuid'
 import { isTabDisabled } from '../helpers/utils'
 
